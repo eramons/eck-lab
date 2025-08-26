9363c7ca1d8d88d5c54f6faa87f6e1eaa98269ed
Eva Ramon <eva.ramon@elastic.co>
2025-08-26 10:21:45 +0200

    ArgoCD installation set to manual

--- terraform/modules/helm/main.tf
+++ terraform/modules/helm/main.tf
@@ -35,16 +35,16 @@ resource "helm_release" "kube_state_metrics" {
   chart      = "kube-state-metrics"
 }
 
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  namespace  = "argocd"
-  create_namespace = true
+#resource "helm_release" "argocd" {
+#  name       = "argocd"
+#  namespace  = "argocd"
+#  create_namespace = true
 
-  repository = "https://argoproj.github.io/argo-helm"
-  chart      = "argo-cd"
-  version    = "5.51.6" 
+#  repository = "https://argoproj.github.io/argo-helm"
+#  chart      = "argo-cd"
+#  version    = "5.51.6" 
 
-  values = [
-    file("${path.module}/values/argocd.yaml")
-  ]
-}
+#  values = [
+#    file("${path.module}/values/argocd.yaml")
+#  ]
+#}
